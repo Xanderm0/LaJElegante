8023ebe12b60f7759e25751b78b5e7111a3400dc
Jeremy-Duarte <jeremydamian.db@gmail.com>
2025-12-13 12:01:35 -0500

    feat: Metodo para convertir ENUM a STRING enums.Estado.java

--- src/java/models/enums/Estado.java
+++ src/java/models/enums/Estado.java
@@ -14,4 +14,18 @@ public enum Estado {
     public String getValor() {
         return valor;
     }
+    
+    public static Estado fromString(String valor) {
+        if (valor == null) return null;
+        
+        valor = valor.trim().toLowerCase();
+        
+        for (Estado estado : Estado.values()) {
+            if (estado.valor.equals(valor)) {
+                return estado;
+            }
+        }
+        
+        throw new IllegalArgumentException("Estado no válido: " + valor);
+    }
 }
